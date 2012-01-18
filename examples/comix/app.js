25a7926cdb480bd5d49728dc8db28b80a77104ce
Chris Strom <eee.c@eeecooks.com>
2012-01-17 23:22:09 -0500

    First attempt at in-form XHR

--- examples/comix/app.js
+++ examples/comix/app.js
@@ -30,6 +30,12 @@ app.configure('production', function(){
 
 // Routes
 
+app.get('/', function(req, res){
+  res.render('index', { title: 'Comix' });
+});
+
+
+
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
